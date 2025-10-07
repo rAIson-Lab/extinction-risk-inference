3998dd9384c7cd3f6dfe3127b1fafbeeb4a478ef
pabmevi <pabmevi@gmail.com>
2025-10-07 15:11:13 -0500

    f

--- scripts/updatedextinctionbirds.py
+++ scripts/updatedextinctionbirds.py
@@ -10,7 +10,7 @@ from datasets import updated_extinction_birds # Our new function
 # Load the data
 
 # Cargar y filtrar los datos para usar solo 0 y 1 en extinction_risk
-model_template, data = updated_extinction_birds(data_path='datasets/Extinction/Avo_Birdbase.csv')
+model_template, data = updated_extinction_birds()
 label_index = model_template.attrs.index(model_template.label) if model_template.label in model_template.attrs else -1
 data = [row for row in data if str(row[label_index]) in ['Lower_risk', 'Higher_risk']]
 
