3fb6c4c3d58cd4cb3ed457b6836ef3d7e5cfe61e
pabmevi <pabmevi@gmail.com>
2025-10-07 13:05:05 -0500

    adding modified db

--- datasets.py
+++ datasets.py
@@ -25,14 +25,14 @@ def updated_extinction_birds(data_path='datasets/Extinction/Avo_Birdbase_noNA.cs
              "Tarsus.Length","Wing.Length","Kipps.Distance","Secondary1","Hand.Wing.Index",
              "Tail.Length","Habitat.Density","Migration","Primary.Lifestyle","Min.Latitude",
              "Max.Latitude","Centroid.Latitude","Centroid.Longitude","Range.Size","IUCN2024",
-             "RR","ISL","RLM","LAT","NormMin","Elevational.Range","NormMax","HB","DB",
+             "RR","ISL","RLM","LAT","Elevational.Range","HB","DB",
              "Nest_Type","Nest_SBS","Clutch_Max","Flightlessness","BodyMass","Order2",
              "Family","Habitat1","Diet"]
     
     nums = ["Beak.Length_Culmen","Beak.Length_Nares","Beak.Width","Beak.Depth",
              "Tarsus.Length","Wing.Length","Kipps.Distance","Secondary1","Hand.Wing.Index",
              "Tail.Length","Min.Latitude","Max.Latitude","Centroid.Latitude","Centroid.Longitude",
-             "Range.Size","NormMin","Elevational.Range","NormMax","HB","DB","Clutch_Max","BodyMass"]
+             "Range.Size","Elevational.Range","HB","DB","Clutch_Max","BodyMass"]
     label = 'extinction_risk'
     model = Classifier(attrs=attrs, numeric=nums, label=label)
     data = model.load_data(data_path) # Use the argument here
